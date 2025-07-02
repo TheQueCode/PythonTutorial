f7b8c191c5958b50f63869c2e154aacaf0ac2201
TheQueCode <thequecode@gmail.com>
2025-07-01 19:45:41 -0500

    python tut

---
+++ Python/main.py
@@ -0,0 +1,66 @@
+# This is a comment
+print('Hello World!')
+print('I like pizza!')
+print('It is really good!')
+
+# variables - (string, integer, float, boolean)
+
+# Strings
+first_name = 'Que'
+food = 'pizza'
+email = 'quecode@gmail.com'
+
+print(first_name)
+
+#fstring
+print(f'Hello, my name is {first_name}')
+print(f'I like {food}')
+print(f'My email is: {email}')
+
+# Integer - whole numbers
+
+age = 25
+quantity = 3
+num_of_students = 30
+
+print(f"I'm {age} years old.")
+print(f'I am buying {quantity} items.')
+print(f'My class has {num_of_students} students.')
+
+# floats - floating point number
+
+price = 10.99
+gpa = 3.8
+distance = 5.5
+
+print(f'The price is ${price}')
+print(f'My GPA is: {gpa}')
+print(f'I ran {distance} km.')
+
+# Boolean - true or false
+
+is_student = True
+has_kids = False
+
+print(f'I am a student: {is_student}')
+print(f'I have kids: {has_kids}')
+
+if is_student:
+  	print('You are a student')
+else:
+  	print('You are not a student')
+		
+
+for_sale = False
+
+if for_sale:
+	print('This item is for sale')
+else:
+	print('This item is not for sale')
+
+is_online = True
+
+if is_online:
+	print('Is online')
+else:
+	print('Is offline')
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PythonTutorial
+
 Python Tutorial
+
+https://www.youtube.com/watch?v=ix9cRaBkVe0
+
+Python & JS
+
+https://www.youtube.com/watch?v=PppslXOR7TA
