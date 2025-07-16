e40152b73d4fa2ce785004126866f01c00e3a05c
TheQueCode <thequecode@gmail.com>
2025-07-16 16:49:52 -0500

    Python functions

--- Python/python_sandbox/functions.py
+++ Python/python_sandbox/functions.py
@@ -1,5 +1,26 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
+# Create a function
+def sayHello(name='Sam'):
+    print(f'Hello {name}')
 
-# A lambda function is a small anonymous function.
-# A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
+
+sayHello('Que Code')
+sayHello()
+
+# Return values
+
+
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
+
+
+num = getSum(3, 4)
+print(num)
+
+# A lambda function is a small anonymous function. A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
+
+# getSum = lambda num1, num2: num1 + num2
+
+# print(getSum(10, 3))
