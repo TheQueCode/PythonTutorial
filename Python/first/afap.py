4e643e71115926bd2a3c97e58e206df0753c4815
TheQueCode <thequecode@gmail.com>
2025-07-17 23:44:51 -0500

    Arithmetic Operators

---
+++ Python/first/afap.py
@@ -0,0 +1,65 @@
+# Core Data Types: Int, Float, String, Bool
+
+# Int - any whole number without a decimal
+
+-2394849583
+2090293
+1
+-1
+5743
+
+# Float
+
+-9.72
+27272.0
+897.987
+
+# String
+
+'Hello'
+"Hello"
+'4.6'
+
+# Bool
+
+True - 1
+False - 0
+
+
+# Output and Printing
+print('Hello World!')
+print(4.5)
+print(1110.0111, 'hello', True, 'Now', -88)
+
+# Variables
+hello = 'que'
+world = 'world'
+world = hello
+hello = 'no'
+
+print(hello, world)
+
+# User Input
+# name = input('Name: ')
+# age = input('Age: ')
+
+# print('Hello', name, 'you are', age, 'years old')
+# print(f'Hello, {name}')
+
+# Arithmetic Operators
+x = 10
+y = 3
+result = x + y
+result = x - y
+result = x * y
+result = int(x / y)
+result = x ** y
+# Floor division - removes repeating decimal point. Answer isn't 3.3333333, it's 3
+result = x // y
+result = x % y
+
+# Order of operation - BEDMAS - Brackets, Exponents, Division, Multiplication, Addition, Subtraction
+print(result)
+
+num = input('Number: ')
+print(int(num)-5)
